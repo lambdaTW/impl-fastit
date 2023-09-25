ac83471ce211641d9faadcaec60dfe91c8ba55ee
lambdaTW <lambda@lambda.tw>
2023-09-25 20:49:38 +0800

    feat: get username from token info

--- src/tests/test_services/test_token.py
+++ src/tests/test_services/test_token.py
@@ -12,5 +12,11 @@ async def test_create_jwt_token_by_username_and_passowrd():
         )
         assert resp.status_code == 200
         data = resp.json()
-        assert data["access_token"]
+        assert (access_token := data["access_token"])
         assert data["refresh_token"]
+        resp = await client.get(
+            "/v1/auth/users/tokens/info",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        assert resp.status_code == 200
+        assert resp.json()["username"] == "test"
