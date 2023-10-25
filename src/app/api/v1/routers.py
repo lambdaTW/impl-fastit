28e482126b1cfbcd931ee4c4b79bb9cd14f1d8ee
lambdaTW <lambda@lambda.tw>
2023-10-25 18:39:59 +0800

    style: fix lint

--- src/app/api/v1/routers.py
+++ src/app/api/v1/routers.py
@@ -1,8 +1,8 @@
 import fastapi
 
-from app.api.v1.endpoints.auth.users import users as users_endpoints
 from app.api.v1.endpoints.auth.users import hashes as hashes_endpoints
 from app.api.v1.endpoints.auth.users import tokens as token_endpoints
+from app.api.v1.endpoints.auth.users import users as users_endpoints
 
 v1_router = fastapi.APIRouter()
 v1_router.include_router(users_endpoints.router, prefix="/auth/users")
