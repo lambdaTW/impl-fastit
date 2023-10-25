2ddb655231ba4709ea01f83792b32d3c4567c20c
lambdaTW <lambda@lambda.tw>
2023-10-25 18:30:10 +0800

    feat: block none superuser permission client to create a user
    test: user cannot create superuser
    
    test: anonymous cannot create superuser

--- src/app/api/v1/endpoints/auth/users/users.py
+++ src/app/api/v1/endpoints/auth/users/users.py
@@ -1,17 +1,42 @@
 import fastapi
+import jose
+from fastapi import security
+from jose import jwt
 from sqlalchemy.ext import asyncio as sqlalchemy_asyncio
 
 from app.api import dependencies
 from app.crud import auth as auth_crud
 from app.schemas import users as users_schemas
+from core import config
 
 router = fastapi.APIRouter()
 
 
 @router.post("", status_code=201)
 async def create_user(
+    token: str = fastapi.Depends(security.OAuth2PasswordBearer(tokenUrl="token")),
     user_info: users_schemas.UserInfo = fastapi.Body(),
     db: sqlalchemy_asyncio.AsyncSession = fastapi.Depends(dependencies.get_db),
 ):
+    credentials_exception = fastapi.HTTPException(
+        status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token,
+            config.Settings().authjwt_secret_key,
+            algorithms=["HS256"],
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jose.JWTError:
+        raise credentials_exception
+    action_user = await auth_crud.user.get_by_username(db, username)
+
+    if not action_user.is_superuser:
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_403_FORBIDDEN)
     user = await auth_crud.user.create(db, user_info)
     return user.__dict__
--- src/tests/test_services/test_users.py
+++ src/tests/test_services/test_users.py
@@ -39,16 +39,19 @@ async def superuser_client(
         yield async_client
 
 
+@pytest.mark.parametrize("is_superuser", [True, False])
 async def test_superuser_can_create_user(
+    db: sqlalchemy_asyncio.AsyncSession,
     client: httpx.AsyncClient,
     superuser_client: httpx.AsyncClient,
+    is_superuser: bool,
 ):
     username = str(uuid.uuid4())
     password = str(uuid.uuid4())
     user_info = {
         "username": username,
         "password": password,
-        "is_superuser": False,
+        "is_superuser": is_superuser,
     }
     resp = await superuser_client.post("/v1/auth/users", json=user_info)
     assert resp.status_code == 201
@@ -57,3 +60,62 @@ async def test_superuser_can_create_user(
         json=user_info,
     )
     assert resp.status_code == 200
+    user = await auth_crud.user.get_by_username(db, username)
+    assert user.is_superuser is user_info["is_superuser"]
+
+
+@pytest.fixture
+async def user(migrate: None, db: sqlalchemy_asyncio.AsyncSession) -> auth_models.User:
+    return await auth_crud.user.create(
+        db,
+        {
+            "username": str(uuid.uuid4()),
+            "password": str(uuid.uuid4()),
+            "is_superuser": False,
+        },
+    )
+
+
+@pytest.fixture
+async def user_client(
+    user: auth_models.User,
+) -> typing.AsyncIterator[httpx.AsyncClient]:
+    from app.api.v1.endpoints.auth.users import tokens
+
+    async with httpx.AsyncClient(
+        app=main.app,
+        base_url="http://test",
+    ) as async_client:
+        token = tokens.create_access_token(dict(sub=user.username))
+        async_client.headers = {"authorization": f"Bearer {token}"}
+        yield async_client
+
+
+async def test_user_cannot_create_superuser(
+    db: sqlalchemy_asyncio.AsyncSession,
+    user_client: httpx.AsyncClient,
+):
+    username = str(uuid.uuid4())
+    password = str(uuid.uuid4())
+    user_info = {
+        "username": username,
+        "password": password,
+        "is_superuser": True,
+    }
+    resp = await user_client.post("/v1/auth/users", json=user_info)
+    assert resp.status_code == 403
+
+
+async def test_anonymous_cannot_create_superuser(
+    db: sqlalchemy_asyncio.AsyncSession,
+    client: httpx.AsyncClient,
+):
+    username = str(uuid.uuid4())
+    password = str(uuid.uuid4())
+    user_info = {
+        "username": username,
+        "password": password,
+        "is_superuser": True,
+    }
+    resp = await client.post("/v1/auth/users", json=user_info)
+    assert resp.status_code == 401
